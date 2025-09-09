7b36878c38e9a5cf0ba630718dfe7d79bd06e577
Elena <halvushka@gmail.com>
2025-09-09 12:27:56 -0400

    edit

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,6 +1,9 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.myText)
+        val editText = findViewById<EditText>(R.id.editTextText)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+
+            if (editText.text.isNotEmpty()) {
+
+                textView.text = "Hello, " + EditText.text + "! Welcome"
+
+                else {
+
+                        textView.text = "Please enter your name"
+
+                }
+
+            }
+
+        }
+
     }
 }
